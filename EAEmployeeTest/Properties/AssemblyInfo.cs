281d5a91e3e203d6c3016a156b5652e0223167ab
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-04-16 15:12:14 +0200

    16. Writing a simple code with Selenium

---
+++ EAEmployeeTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EAEmployeeTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EAEmployeeTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("044f0a9c-0449-4465-9fee-835aa85b00ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EAEmployeeTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace EAEmployeeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string url = "http://localhost:64429/";
+        private IWebDriver _driver;
+
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Navigate().GoToUrl(url);
+
+            Login();
+
+        }
+
+        public void Login()
+        {
+            _driver.FindElement(By.LinkText("Login")).Click();
+
+            //UserName
+            _driver.FindElement((By.Id("UserName"))).SendKeys("admin");
+            _driver.FindElement((By.Id("Password"))).SendKeys("password");
+
+            //click Login
+            _driver.FindElement(By.CssSelector("input.btn")).Submit();
+        }
+    }
+}
