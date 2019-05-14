63212b900d4a526461763f637309fa90b66f8082
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-05-14 16:59:03 +0200

    34-35. Database Extension Helper

--- EATestProject/Helpers/DataHelperExtentions.cs
+++ EATestProject/Helpers/DataHelperExtentions.cs
@@ -1,12 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EAAutoFramework.Helpers
 {
-    class DataHelperExtentions
+    public static class DataHelperExtentions
     {
+        //Open the connection
+        public static SqlConnection DBConnect(this SqlConnection sqlConnection, string connectionString)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+            catch (Exception e)
+            {
+                LogHelpers.Write("ERROR :: " + e.Message);
+            }
+            return null;
+        }
+
+        //Closing the connection
+        public static void DBClose(this SqlConnection sqlConnection)
+        {
+            try
+            {
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                LogHelpers.Write("ERROR :: " + e.Message);
+            }
+        }
+
+        //Execution
+        public static DataTable ExecuteQuery(this SqlConnection sqlConnection, string queryString)
+        {
+            DataSet dataset;
+            try
+            {
+                if (sqlConnection == null || ((sqlConnection != null && (sqlConnection.State == ConnectionState.Closed ||
+                    sqlConnection.State == ConnectionState.Broken))))
+                    sqlConnection.Open();
+
+                SqlDataAdapter dataAdaptor = new SqlDataAdapter();
+                dataAdaptor.SelectCommand = new SqlCommand(queryString, sqlConnection);
+                dataAdaptor.SelectCommand.CommandType = CommandType.Text;
+
+                dataset = new DataSet();
+                dataAdaptor.Fill(dataset, "table");
+                sqlConnection.Close();
+                return dataset.Tables["table"];
+            }
+            catch(Exception e)
+            {
+                dataset = null;
+                sqlConnection.Close();
+                LogHelpers.Write("ERROR :: " + e.Message);
+                return null;
+            }
+            finally
+            {
+                sqlConnection.Close();
+                dataset = null;
+            }
+        }        
     }
 }
