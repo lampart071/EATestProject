541c440acd97deef9f4995dc9221dc6834629d8f
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-06-18 15:48:55 +0200

    44. Web Element Extension (Contd)

--- EATestProject/Extentions/WebElementExtentions.cs
+++ EATestProject/Extentions/WebElementExtentions.cs
@@ -1,4 +1,6 @@
-﻿using OpenQA.Selenium;
+﻿using EAAutoFramework.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,24 @@ namespace EAAutoFramework.Extentions
 {
     public static class WebElementExtentions
     {
+        public static string GetSelectedDropDown(this IWebElement element)
+        {
+            SelectElement ddl = new SelectElement(element);
+            return ddl.AllSelectedOptions.First().ToString();
+        }
+
+        public static IList<IWebElement> GetSelectedListOptions(this IWebElement element)
+        {
+            SelectElement ddl = new SelectElement(element);
+            return ddl.AllSelectedOptions;
+        }
+
+        public static void Hover(this IWebElement element)
+        {
+            Actions actions = new Actions(DriverContext.Driver);
+            actions.MoveToElement(element).Perform();
+        }
+
         public static void SelectDropDownList(this IWebElement element, string value)
         {
             SelectElement ddl = new SelectElement(element);
