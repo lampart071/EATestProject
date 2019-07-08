02b54b02c05adca0d6f91f5eff82625971eacaaf
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-07-08 09:56:39 +0200

    Added ControlFile

---
+++ EATestProject/ControlFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAAutoFramework
+{
+    class ControlFile
+    {
+
+        // Sandbox repair-after-first-pr before merge with master
+
+
+    }
+}
