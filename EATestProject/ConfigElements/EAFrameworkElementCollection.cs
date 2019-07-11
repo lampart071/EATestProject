dedbab58865a0e479b03f9ffb109a60f046d39fd
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-07-11 17:11:28 +0200

    68-69. Developing configuration with configuration manager (ConfigurationElement)

---
+++ EATestProject/ConfigElements/EAFrameworkElementCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAAutoFramework.ConfigElements
+{
+    [ConfigurationCollection(typeof(EAFrameworkElement), AddItemName = "testSetting", CollectionType = ConfigurationElementCollectionType.BasicMap)]
+    public class EAFrameworkElementCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new EAFrameworkElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return (element as EAFrameworkElement).Name;
+        }
+
+        public new EAFrameworkElement this[string type]
+        {
+            get
+            {
+                return (EAFrameworkElement)base.BaseGet(type);
+            }
+        }
+    }
+}
---
+++ EATestProject/ConfigElements/EATestConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAAutoFramework.ConfigElements
+{
+    public class EATestConfiguration : ConfigurationSection
+    {
+        private static EATestConfiguration _testConfig = (EATestConfiguration)ConfigurationManager.GetSection("EATestConfiguration");
+
+        public static EATestConfiguration EASettings { get { return _testConfig; } }
+
+        [ConfigurationProperty("testSettings")]
+        public EAFrameworkElementCollection TestSettings { get { return (EAFrameworkElementCollection)base["testSettings"]; } }
+
+    }
+}
