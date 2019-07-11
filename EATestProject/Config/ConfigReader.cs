9de3c20cd805c786b788bde584fddc77851ada6c
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-07-11 17:24:41 +0200

    71. Calling custom configuration in framework - To fix. Tests don't run yet.

--- EATestProject/Config/ConfigReader.cs
+++ EATestProject/Config/ConfigReader.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Xml.XPath;
+using EAAutoFramework.ConfigElements;
 
 namespace EAAutoFramework.Config
 {
@@ -9,39 +10,14 @@ namespace EAAutoFramework.Config
     {
         public static void SetFrameworkSettings()
         {
-            XPathItem aut;
-            XPathItem buildname;
-            XPathItem browser;
-            XPathItem testtype;
-            XPathItem islog;
-            XPathItem isreport;
-            XPathItem logPath;
-            XPathItem appConnection;
-
-            string strFilename = Environment.CurrentDirectory.ToString() + "\\Config\\GlobalConfig.xml";
-            FileStream stream = new FileStream(strFilename, FileMode.Open);
-            XPathDocument document = new XPathDocument(stream);
-            XPathNavigator navigator = document.CreateNavigator();
-
-            //Get XML Details and pass it in XPathItem type variables
-            aut = navigator.SelectSingleNode("EAAutoFramework/RunSettings/AUT");
-            buildname = navigator.SelectSingleNode("EAAutoFramework/RunSettings/BuildName");
-            browser = navigator.SelectSingleNode("EAAutoFramework/RunSettings/Browser");
-            testtype = navigator.SelectSingleNode("EAAutoFramework/RunSettings/TestType");
-            islog = navigator.SelectSingleNode("EAAutoFramework/RunSettings/IsLog");
-            isreport = navigator.SelectSingleNode("EAAutoFramework/RunSettings/IsReport");
-            logPath = navigator.SelectSingleNode("EAAutoFramework/RunSettings/LogPath");
-            appConnection = navigator.SelectSingleNode("EAAutoFramework/RunSettings/ApplicationDB");
-
-            //Set XML Details in the property to be used accross framework
-            Settings.AUT = aut.Value.ToString();
-            Settings.BuildName = buildname.Value.ToString();
-            Settings.BrowserType = (BrowserType)Enum.Parse(typeof(BrowserType), browser.Value.ToString());
-            Settings.TestType = testtype.Value.ToString();
-            Settings.IsLog = islog.Value.ToString();
-            Settings.IsReporting = isreport.Value.ToString();            
-            Settings.LogPath = logPath.Value.ToString();
-            Settings.AppConnectionString = appConnection.Value.ToString();
+            Settings.AUT = EATestConfiguration.EASettings.TestSettings["staging"].AUT;
+            //Settings.BuildName = buildname.Value.ToString();
+            Settings.TestType = EATestConfiguration.EASettings.TestSettings["staging"].TestType;
+            Settings.IsLog = EATestConfiguration.EASettings.TestSettings["staging"].IsLog;
+            //Settings.IsReporting = EATestConfiguration.EASettings.TestSettings["staging"].IsReadOnly;
+            Settings.LogPath = EATestConfiguration.EASettings.TestSettings["staging"].LogPath;
+            //Settings.AppConnectionString = appConnection.Value.ToString();
+            Settings.BrowserType = (BrowserType)Enum.Parse(typeof(BrowserType), EATestConfiguration.EASettings.TestSettings["staging"].Browser);
         }
     }
 }
