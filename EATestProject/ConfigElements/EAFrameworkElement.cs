9727b45c57e6af263c00f11c4a5c57cea6f6566e
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-07-13 12:43:27 +0200

    mod defaults in ConfigurationElement

--- EATestProject/ConfigElements/EAFrameworkElement.cs
+++ EATestProject/ConfigElements/EAFrameworkElement.cs
@@ -22,14 +22,14 @@ namespace EAAutoFramework.ConfigElements
         //[ConfigurationProperty("logPath", IsRequired = true)]
         //public string LogPath { get { return (string)base["logPath"]; } }
 
-        [ConfigurationProperty("name", DefaultValue = "", IsKey = true, IsRequired = true)]
+        [ConfigurationProperty("name", DefaultValue = "NO_NAME", IsKey = true, IsRequired = true)]
         public string name
         {
             get { return (string)base["name"]; }
             set { base["name"] = value; }
         }
 
-        [ConfigurationProperty("userid", DefaultValue = "abhi", IsKey = false, IsRequired = false)]
+        [ConfigurationProperty("userid", DefaultValue = "NO_USER", IsKey = false, IsRequired = false)]
         public string userid
         {
             get { return (string)base["userid"]; }
