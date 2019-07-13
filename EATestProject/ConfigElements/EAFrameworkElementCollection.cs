41765e8b1885b19fd898ff8153fbc5ac2edd9aa0
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-07-13 13:07:51 +0200

    mod for ConfigurationElementCollection

--- EATestProject/ConfigElements/EAFrameworkElementCollection.cs
+++ EATestProject/ConfigElements/EAFrameworkElementCollection.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 
 namespace EAAutoFramework.ConfigElements
 {
@@ -7,39 +8,28 @@ namespace EAAutoFramework.ConfigElements
     public class EAFrameworkElementCollection : ConfigurationElementCollection
     {
 
+        //Settings from outer project
         internal const string PropertyName = "Element";
-
-        protected override ConfigurationElement CreateNewElement()
-        {
-            return new EAFrameworkElement();
-        }
-
-        protected override ConfigurationElement CreateNewElement() { return new Element(); }
-
-        protected override object GetElementKey(ConfigurationElement element)
-        {
-            return (element as EAFrameworkElement).Name;
-        }
-
-        protected override object GetElementKey(ConfigurationElement element) { return ((Element)(element)).name; }
-
-        public new EAFrameworkElement this[string type]
-        {
-            get
-            {
-                return (EAFrameworkElement)base.BaseGet(type);
-            }
-        }
-
-
-
-        
         public override ConfigurationElementCollectionType CollectionType { get { return ConfigurationElementCollectionType.BasicMapAlternate; } }
         protected override string ElementName { get { return PropertyName; } }
         protected override bool IsElementName(string elementName) { return elementName.Equals(PropertyName, StringComparison.InvariantCultureIgnoreCase); }
         public override bool IsReadOnly() { return false; }
+
+
+        //Similarities
+        protected override ConfigurationElement CreateNewElement(){return new EAFrameworkElement();} //OK        
+        protected override object GetElementKey(ConfigurationElement element) { return ((EAFrameworkElement)(element)).name; } //OK
+
         
-        
-        public Element this[int idx] { get { return (Element)BaseGet(idx); } }
+        //public new EAFrameworkElement this[string type]{get{return (EAFrameworkElement)base.BaseGet(type);}}//? To decide
+        public EAFrameworkElement this[int idx] { get { return (EAFrameworkElement)BaseGet(idx); } }//? To decide
+
+
+
+
+
+
+
+
     }
 }   
