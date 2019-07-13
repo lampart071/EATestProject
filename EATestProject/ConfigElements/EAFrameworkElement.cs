c982e126d2841ff42cb692ce81246638b198b77c
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-07-13 12:42:06 +0200

    simplified version for ConfigurationElement

--- EATestProject/ConfigElements/EAFrameworkElement.cs
+++ EATestProject/ConfigElements/EAFrameworkElement.cs
@@ -4,22 +4,37 @@ namespace EAAutoFramework.ConfigElements
 {
     public class EAFrameworkElement : ConfigurationElement
     {
-        [ConfigurationProperty("name", IsRequired = true)]
-        public string Name { get { return (string)base["name"]; } }
+        //[ConfigurationProperty("name", IsRequired = true)]
+        //public string Name { get { return (string)base["name"]; } }
 
-        [ConfigurationProperty("aut", IsRequired = true)]
-        public string AUT { get { return (string)base["aut"]; } }
+        //[ConfigurationProperty("aut", IsRequired = true)]
+        //public string AUT { get { return (string)base["aut"]; } }
 
-        [ConfigurationProperty("browser", IsRequired = true)]
-        public string Browser { get { return (string)base["browser"]; } }
+        //[ConfigurationProperty("browser", IsRequired = true)]
+        //public string Browser { get { return (string)base["browser"]; } }
 
-        [ConfigurationProperty("testType", IsRequired = true)]
-        public string TestType { get { return (string)base["testType"]; } }
+        //[ConfigurationProperty("testType", IsRequired = true)]
+        //public string TestType { get { return (string)base["testType"]; } }
 
-        [ConfigurationProperty("isLog", IsRequired = true)]
-        public string IsLog { get { return (string)base["isLog"]; } }
+        //[ConfigurationProperty("isLog", IsRequired = true)]
+        //public string IsLog { get { return (string)base["isLog"]; } }
 
-        [ConfigurationProperty("logPath", IsRequired = true)]
-        public string LogPath { get { return (string)base["logPath"]; } }
+        //[ConfigurationProperty("logPath", IsRequired = true)]
+        //public string LogPath { get { return (string)base["logPath"]; } }
+
+        [ConfigurationProperty("name", DefaultValue = "", IsKey = true, IsRequired = true)]
+        public string name
+        {
+            get { return (string)base["name"]; }
+            set { base["name"] = value; }
+        }
+
+        [ConfigurationProperty("userid", DefaultValue = "abhi", IsKey = false, IsRequired = false)]
+        public string userid
+        {
+            get { return (string)base["userid"]; }
+            set { base["userid"] = value; }
+        }
+
     }
 }
