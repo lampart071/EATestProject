26950082df2a3e74049063d21120e2d45bce5c18
Michal Lamperski <Michal.Lamperski@cdsazure.onmicrosoft.com>
2019-07-17 14:46:22 +0200

    94. Complete Execution of Test with Parallel support. TOFIX: LogHelper, ExtentReports, Klov, NUnit Runner, Paraller run, Browser settings.

--- EATestProject/Helpers/LogHelpers.cs
+++ EATestProject/Helpers/LogHelpers.cs
@@ -12,25 +12,25 @@ namespace EAAutoFramework.Helpers
         // Create a file which can store the log information
         public static void CreateLogFile()
         {
-            //MY: Directory is different than in the course
-            string dir = Environment.CurrentDirectory.ToString() + "\\Data\\";
-            if (Directory.Exists(dir))
-            {
-                _streamw = File.AppendText(dir + _logFileName + ".log");
-            }
-            else
-            {
-                Directory.CreateDirectory(dir);
-                _streamw = File.AppendText(dir + _logFileName + ".log");
-            }
+            ////MY: Directory is different than in the course
+            //string dir = "C:\\DEVLOGS\\";
+            //if (Directory.Exists(dir))
+            //{
+            //    _streamw = File.AppendText(dir + _logFileName + ".log");
+            //}
+            //else
+            //{
+            //    Directory.CreateDirectory(dir);
+            //    _streamw = File.AppendText(dir + _logFileName + ".log");
+            //}
         }
         
         // Create a method which can write the text in the log file
         public static void Write(string logMessage)
         {
-            _streamw.Write("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongTimeString());
-            _streamw.WriteLine("    {0}", logMessage);
-            _streamw.Flush();
+            //_streamw.Write("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongTimeString());
+            //_streamw.WriteLine("    {0}", logMessage);
+            //_streamw.Flush();
         }
         public static void CloseLog()
         {
